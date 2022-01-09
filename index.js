bd2a4c2ac072dc6eeb398077e008debb0b29ffa5
ramki45 <89958455+ramki45@users.noreply.github.com>
2022-01-09 13:45:16 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const fs = require('fs')
+const express = require('express');
+const app = express();
+
+
+app.post('/createfile', function (req, res) {
+    var timestamp = new Date().toISOString();
+    var filename = `${timestamp}.txt`
+    var fileContent = `${timestamp} - timestamp created`
+    fs.writeFile(filename, fileContent, function (err, data) {
+        if (err) throw err;
+        console.log('filecreated')
+    })
+    res.json({ "message": "success" })
+})
+app.listen(8000)
